6819d71d75014f04504842c7cb25d4edbfb323e2
Rodrigo Peleias <rodrigo.peleias@gmail.com>
2020-03-29 12:49:11 -0300

    Project setup

---
+++ src/main/java/com/rodrigopeleias/bookstoremanager/BookstoreManagerFreeVersionApplication.java
@@ -0,0 +1,13 @@
+package com.rodrigopeleias.bookstoremanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreManagerFreeVersionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreManagerFreeVersionApplication.class, args);
+	}
+
+}
