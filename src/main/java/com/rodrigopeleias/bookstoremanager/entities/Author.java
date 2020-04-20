26b569dbd19b0005134563a475f0982778ebf913
Rodrigo Peleias <rodrigo.peleias@gmail.com>
2020-04-19 22:25:25 -0300

    Removed unused classes

--- src/main/java/com/rodrigopeleias/bookstoremanager/entities/Author.java
+++ src/main/java/com/rodrigopeleias/bookstoremanager/entities/Author.java
@@ -6,16 +6,10 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
-import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.PostUpdate;
-import javax.persistence.PrePersist;
-import java.time.LocalDateTime;
-import java.util.List;
 
 @Entity
 @Data
